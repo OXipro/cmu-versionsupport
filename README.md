5de2578804d8932da78575873d9042f38c86cadf
andrei1058 <andrew.dascalu@gmail.com>
2019-10-19 19:21:10 +0200

    added block support

--- README.md
+++ README.md
@@ -61,3 +61,27 @@ public class Main extends JavaPlugin {
     }   
 }
 ```
+
+### Block Support
+
+```xml
+<repository>
+  <id>version-support</id>
+  <url>https://gitlab.com/api/v4/projects/14877570/packages/maven</url>
+</repository>
+```
+```xml
+<dependency>
+  <groupId>com.andrei1058.spigot.versionsupport</groupId>
+  <artifactId>block-version</artifactId>
+  <version>[1.0,)</version>
+</dependency>
+```
+
+```java
+public class Main extends JavaPlugin {
+    public void onEnable(){
+        BlockSupport blockSupport = BlockSupport.SupportBuilder.load();
+    }   
+}
+```
---
+++ block_common/src/main/java/com/andrei1058/spigot/versionsupport/BlockSupport.java
@@ -0,0 +1,58 @@
+package com.andrei1058.spigot.versionsupport;
+
+import org.bukkit.Bukkit;
+import org.bukkit.block.Block;
+
+import javax.annotation.Nullable;
+import java.lang.reflect.InvocationTargetException;
+
+public interface BlockSupport {
+
+    /**
+     * Change a block data.
+     * Used for versions under 1.13.
+     *
+     * @param block target.
+     * @param data  block data.
+     */
+    void setBlockData(Block block, byte data);
+
+    /**
+     * Change a block durability.
+     *
+     * @param oldName    vanilla block name for versions before 1.13.
+     * @param new_v1_13  enum name for 1.13+.
+     * @param durability durability.
+     */
+    void setDurability(String oldName, String new_v1_13, float durability);
+
+    /**
+     * @param sign sign block.
+     * @return the block behind a sign. Null if given block is not a sign.
+     */
+    @Nullable
+    Block getBlockBehindSign(Block sign);
+
+    class SupportBuilder {
+
+        /**
+         * @return block support for your server version. Null if not supported.
+         */
+        @Nullable
+        public static BlockSupport load() {
+            String version = Bukkit.getServer().getClass().getName().split(".")[3];
+            Class c;
+            try {
+                c = Class.forName("com.andrei1058.spigot.versionsupport.block." + version);
+            } catch (ClassNotFoundException e) {
+                //I can't run on your version
+                return null;
+            }
+            try {
+                return (BlockSupport) c.getConstructors()[0].newInstance();
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                return null;
+            }
+        }
+    }
+}
