c0130d2b58eade607d2c675c385c1a185e732eb1
andrei1058 <andrew.dascalu@gmail.com>
2020-02-06 15:58:20 +0100

    added title api

---
+++ titlecommon/src/main/java/com/andrei1058/spigot/versionsupport/TitleSupport.java
@@ -0,0 +1,49 @@
+package com.andrei1058.spigot.versionsupport;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public interface TitleSupport {
+
+    /**
+     * Send a title or a subtitle.
+     *
+     * @param p        target player.
+     * @param title    msg, null if you want just a subtitle.
+     * @param subtitle msg, null if you want just a title.
+     * @param fadeIn   fade in in ticks.
+     * @param fadeOut  fade out in ticks.
+     * @param stay     keep msg in ticks.
+     */
+    void sendTitle(Player p, String title, String subtitle, int fadeIn, int stay, int fadeOut);
+
+    /**
+     * Send an action bar message.
+     *
+     * @param p    target player.
+     * @param text message.
+     */
+    void playAction(Player p, String text);
+
+    class SupportBuilder {
+
+        /**
+         * @return block support for your server version. Null if not supported.
+         */
+        public static TitleSupport load() {
+            String version = Bukkit.getServer().getClass().getName().split("\\.")[3];
+            Class c;
+            try {
+                c = Class.forName("com.andrei1058.spigot.versionsupport.title_" + version);
+            } catch (ClassNotFoundException e) {
+                //I can't run on your version
+                return null;
+            }
+            try {
+                return (TitleSupport) c.newInstance();
+            } catch (InstantiationException | IllegalAccessException e) {
+                return null;
+            }
+        }
+    }
+}
