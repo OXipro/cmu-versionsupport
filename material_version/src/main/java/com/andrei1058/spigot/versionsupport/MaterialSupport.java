c41d7fc5eea77002848f809f19942dc134f945db
Andrei Dascălu <andrew.dascalu@gmail.com>
2020-09-25 23:15:35 +0000

    release 1.5.0

--- material_version/src/main/java/com/andrei1058/spigot/versionsupport/MaterialSupport.java
+++ material_version/src/main/java/com/andrei1058/spigot/versionsupport/MaterialSupport.java
@@ -9,12 +9,16 @@ import java.lang.reflect.InvocationTargetException;
 public interface MaterialSupport {
 
     /**
+     * Check if the given string is a valid material for the current server version.
+     *
      * @param name material name.
      * @return true if material is valid.
      */
     boolean isMaterial(String name);
 
     /**
+     * Get material by name.
+     *
      * @param name material name.
      * @return null if material name is invalid.
      */
@@ -22,6 +26,8 @@ public interface MaterialSupport {
     Material getMaterial(String name);
 
     /**
+     * Get material by name or fallback material.
+     *
      * @param name        material name.
      * @param alternative alternative material.
      * @return alternative if name is null.
@@ -29,42 +35,56 @@ public interface MaterialSupport {
     Material getMaterialOr(String name, Material alternative);
 
     /**
+     * Check if the given material is wool.
+     *
      * @param material target material.
      * @return true if given material is wool.
      */
     boolean isWool(Material material);
 
     /**
+     * Check if the given material is a bed.
+     *
      * @param material target material.
      * @return true if given material is bed.
      */
     boolean isBed(Material material);
 
     /**
+     * Check if the given material is glass.
+     *
      * @param material target material.
      * @return true if given material is glass.
      */
     boolean isGlass(Material material);
 
     /**
+     * Check if the given material is a glass pane.
+     *
      * @param material target material.
      * @return true if given material is glass pane.
      */
     boolean isGlassPane(Material material);
 
     /**
+     * Check if the given material is terracotta.
+     *
      * @param material target material.
      * @return true if given material is stained clay.
      */
     boolean isTerracotta(Material material);
 
     /**
+     * Check if the given material is concrete.
+     *
      * @param material target material.
      * @return true if given material is concrete.
      */
     boolean isConcrete(Material material);
 
     /**
+     * Check if the given material is concrete powder.
+     *
      * @param material target material.
      * @return true if given material is concrete powder.
      */
@@ -82,6 +102,8 @@ public interface MaterialSupport {
     Material getForCurrent(String v1_8, String v1_12, String v1_13);
 
     /**
+     * Check if the given material is cake.
+     *
      * @param material target material.
      * @return true if given material is cake.
      */
