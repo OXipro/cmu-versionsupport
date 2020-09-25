7b60d440cac772e016a141625b87f964cb41d6e5
andrei1058 <andrew.dascalu@gmail.com>
2020-09-26 00:18:15 +0200

    docs update

--- docs_upload.sh
+++ docs_upload.sh
@@ -2,28 +2,28 @@
 apt-get update && apt-get install -y -qq lftp
 
 # block support upload
-lftp -c "set ftp:ssl-allow no; open -u $APIDOCS_USER,$APIDOCS_PASS andrei1058.com; mirror -Rev block_common/target/apidocs ./$CI_PROJECT_TITLE  --ignore-time --parallel=10 --exclude-glob .git* --exclude .git/"
+lftp -c "set ftp:ssl-allow no; open -u $APIDOCS_USER,$APIDOCS_PASS andrei1058.com; mirror -Rev block_common/target/apidocs ./$CI_PROJECT_TITLE/block-version  --ignore-time --parallel=10 --exclude-glob .git* --exclude .git/"
 
 # chat support upload
-lftp -c "set ftp:ssl-allow no; open -u $APIDOCS_USER,$APIDOCS_PASS andrei1058.com; mirror -Rev chat_common/target/apidocs ./$CI_PROJECT_TITLE  --ignore-time --parallel=10 --exclude-glob .git* --exclude .git/"
+lftp -c "set ftp:ssl-allow no; open -u $APIDOCS_USER,$APIDOCS_PASS andrei1058.com; mirror -Rev chat_common/target/apidocs ./$CI_PROJECT_TITLE/chat-version  --ignore-time --parallel=10 --exclude-glob .git* --exclude .git/"
 
 # cmd support upload
-lftp -c "set ftp:ssl-allow no; open -u $APIDOCS_USER,$APIDOCS_PASS andrei1058.com; mirror -Rev cmd_common/target/apidocs ./$CI_PROJECT_TITLE  --ignore-time --parallel=10 --exclude-glob .git* --exclude .git/"
+lftp -c "set ftp:ssl-allow no; open -u $APIDOCS_USER,$APIDOCS_PASS andrei1058.com; mirror -Rev cmd_common/target/apidocs ./$CI_PROJECT_TITLE/cmd-version  --ignore-time --parallel=10 --exclude-glob .git* --exclude .git/"
 
 # item stack support upload
-lftp -c "set ftp:ssl-allow no; open -u $APIDOCS_USER,$APIDOCS_PASS andrei1058.com; mirror -Rev itemstack_common/target/apidocs ./$CI_PROJECT_TITLE  --ignore-time --parallel=10 --exclude-glob .git* --exclude .git/"
+lftp -c "set ftp:ssl-allow no; open -u $APIDOCS_USER,$APIDOCS_PASS andrei1058.com; mirror -Rev itemstack_common/target/apidocs ./$CI_PROJECT_TITLE/itemstack-version  --ignore-time --parallel=10 --exclude-glob .git* --exclude .git/"
 
 # material support upload
-lftp -c "set ftp:ssl-allow no; open -u $APIDOCS_USER,$APIDOCS_PASS andrei1058.com; mirror -Rev material_version/target/apidocs ./$CI_PROJECT_TITLE  --ignore-time --parallel=10 --exclude-glob .git* --exclude .git/"
+lftp -c "set ftp:ssl-allow no; open -u $APIDOCS_USER,$APIDOCS_PASS andrei1058.com; mirror -Rev material_version/target/apidocs ./$CI_PROJECT_TITLE/material-version  --ignore-time --parallel=10 --exclude-glob .git* --exclude .git/"
 
 # particle support upload
-lftp -c "set ftp:ssl-allow no; open -u $APIDOCS_USER,$APIDOCS_PASS andrei1058.com; mirror -Rev particle_common/target/apidocs ./$CI_PROJECT_TITLE  --ignore-time --parallel=10 --exclude-glob .git* --exclude .git/"
+lftp -c "set ftp:ssl-allow no; open -u $APIDOCS_USER,$APIDOCS_PASS andrei1058.com; mirror -Rev particle_common/target/apidocs ./$CI_PROJECT_TITLE/particle-version  --ignore-time --parallel=10 --exclude-glob .git* --exclude .git/"
 
 # sound support upload
-lftp -c "set ftp:ssl-allow no; open -u $APIDOCS_USER,$APIDOCS_PASS andrei1058.com; mirror -Rev sounds_version/target/apidocs ./$CI_PROJECT_TITLE  --ignore-time --parallel=10 --exclude-glob .git* --exclude .git/"
+lftp -c "set ftp:ssl-allow no; open -u $APIDOCS_USER,$APIDOCS_PASS andrei1058.com; mirror -Rev sounds_version/target/apidocs ./$CI_PROJECT_TITLE/sounds-version  --ignore-time --parallel=10 --exclude-glob .git* --exclude .git/"
 
 # title support upload
-lftp -c "set ftp:ssl-allow no; open -u $APIDOCS_USER,$APIDOCS_PASS andrei1058.com; mirror -Rev titlecommon/target/apidocs ./$CI_PROJECT_TITLE  --ignore-time --parallel=10 --exclude-glob .git* --exclude .git/"
+lftp -c "set ftp:ssl-allow no; open -u $APIDOCS_USER,$APIDOCS_PASS andrei1058.com; mirror -Rev titlecommon/target/apidocs ./$CI_PROJECT_TITLE/title-version  --ignore-time --parallel=10 --exclude-glob .git* --exclude .git/"
 
 
 
