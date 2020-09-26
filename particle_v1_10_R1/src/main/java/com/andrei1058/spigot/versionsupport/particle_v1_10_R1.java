8a3a84d0449e8608d9217ad884e45f219f986dc2
andrei1058 <andrew.dascalu@gmail.com>
2020-09-26 14:25:04 +0200

    fixed packages

---
+++ particle_v1_10_R1/src/main/java/com/andrei1058/spigot/versionsupport/particle_v1_10_R1.java
@@ -0,0 +1,9 @@
+package com.andrei1058.spigot.versionsupport;
+
+public class particle_v1_10_R1 implements ParticleSupport {
+
+    @Override
+    public String getForVersion(String v18, String v19, String V12, String V13) {
+        return V12;
+    }
+}
---
+++ particle_v1_16_R1/src/main/java/com/andrei1058/spigot/versionsupport/particle_v1_16_R1.java
@@ -0,0 +1,4 @@
+package com.andrei1058.spigot.versionsupport;
+
+public class particle_v1_16_R1 implements ParticleSupport {
+}
